6d21bcb960dbcf7ce95561bfe8606d8a688372f3
killerwife <killerwife@gmail.com>
2016-08-12 20:57:45 +0200

    Merge backport 407

--- Devildog4355/2.4 progression.sql
+++
@@ -1,5 +0,0 @@
--- Fel Soldier remove
-DELETE FROM creature WHERE guid = 67985;
-DELETE FROM creature_movement WHERE id = 67985;
--- Stormwind construction worker
-DELETE FROM creature WHERE guid = 85178;
--- Devildog4355/Fixes.sql
+++
@@ -1,2 +0,0 @@
--- Fix Error from previous Push I made
-DELETE FROM pool_pool WHERE pool_id = 21317;
