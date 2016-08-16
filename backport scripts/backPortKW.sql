85589470dd19a185ec1067f15511536142702323
killerwife <killerwife@gmail.com>
2016-08-16 20:25:16 +0200

    408 work part 1 KW

---
+++ backport scripts/backPortKW.sql
@@ -0,0 +1,7 @@
+-- UDB_015
+-- Mistress of Deception(q.6568)
+UPDATE quest_template SET OfferRewardText ='Hmph. That one.$B$BHow is Rokaro? As suspicious as ever, sending me a note like this.$B$BYou must be on an important mission if Rokaro has sent you to me.$B$BWell, I owe the Warchief a favor or two.' WHERE entry = 6568; 
+
+-- redundant wrong script
+DELETE FROM dbscripts_on_quest_start WHERE id = 114;
+UPDATE quest_template SET StartScript = 0 WHERE Entry = 114;
