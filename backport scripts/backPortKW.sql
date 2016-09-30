f0332a87798c4f5978f3e32bc4e9324790d40ef9
killerwife <killerwife@gmail.com>
2016-09-30 22:03:39 +0200

    Cleanup before push

--- backport scripts/backPortKW.sql
+++ backport scripts/backPortKW.sql
@@ -10,3 +10,12 @@ UPDATE quest_template SET StartScript = 0 WHERE Entry = 114;
 -- r3153
 UPDATE creature_template SET ScriptName='guard_orgrimmar' WHERE entry=14304;
 UPDATE creature_template SET ScriptName='guard_stormwind' WHERE entry IN (1756);
+
+-- UDB_059
+-- Headless Horseman 
+DELETE FROM creature_equip_template WHERE entry=152;
+INSERT INTO creature_equip_template VALUES(152,13222,0,0);
+UPDATE creature_template SET EquipmentTemplateId = 152 WHERE entry = 23682;
+
+UPDATE quest_template SET SpecialFlags = SpecialFlags|1 WHERE QuestFlags = QuestFlags|4096;
+UPDATE quest_template SET SpecialFlags = SpecialFlags|1 WHERE QuestFlags = QuestFlags|32768;
